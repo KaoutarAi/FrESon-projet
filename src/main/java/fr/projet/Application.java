ea674e4f25361bc34f3f2ece44fcb373a3838c51
Quentin Goutaland <06612219+quentgoutaland@users.noreply.github.com>
2023-04-25 13:00:35 +0200

    debug musique

--- src/main/java/fr/projet/Application.java
+++ src/main/java/fr/projet/Application.java
@@ -1,8 +1,17 @@
 package fr.projet;
 
+import fr.projet.factory.RepositoryFactory;
+import fr.projet.repo.IMusiqueRepository;
+
 public class Application {
 
 	public static void main(String[] args) {
+        // tests musique
+        IMusiqueRepository repoMusique = RepositoryFactory.creaMusiqueRepository();
+
+        repoMusique.findAll()
+                   .stream()
+                   .forEach(mus -> {System.out.println(mus.getId() + " - " + mus.getTitre());});
 	}
 
 }
