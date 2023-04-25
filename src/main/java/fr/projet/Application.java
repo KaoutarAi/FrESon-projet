ba35d7fe7ec16a6b6248c6e6bc443da1bab9949a
Quentin Goutaland <06612219+quentgoutaland@users.noreply.github.com>
2023-04-25 12:36:12 +0200

    musique model classes

--- src/main/java/fr/projet/Application.java
+++ src/main/java/fr/projet/Application.java
@@ -3,8 +3,6 @@ package fr.projet;
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
 
 }
---
+++ src/main/java/fr/projet/model/musique/Album.java
@@ -0,0 +1,31 @@
+package fr.projet.model.musique;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "albums")
+public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
+    private int id;
+
+    @Column(name = "album_name")
+    @Getter @Setter
+    private String nom;
+
+    @OneToMany(mappedBy = "album")
+    @Getter @Setter
+    private List<Musique> musiques;
+
+}
