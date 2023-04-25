ab6064135aca62ad4e5c5c28adbaa0ef4a3d4e49
PierreNicolasRey <pnrey@hotmail.fr>
2023-04-25 11:22:16 +0200

    AbstractRepoJpa

---
+++ src/main/java/fr/projet/repo/jpa/AbstractRepositoryJpa.java
@@ -0,0 +1,8 @@
+package fr.projet.repo.jpa;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public abstract class AbstractRepositoryJpa {
+	protected static EntityManagerFactory emf = Persistence.createEntityManagerFactory("FrESonUnit");
+}
