0a686e893452b6c7c892568dc4021ff78da8f0b4
PierreNicolasRey <pnrey@hotmail.fr>
2023-04-25 11:44:10 +0200

    ajout Connexion/Inscription et checks

---
+++ src/main/java/fr/projet/model/logging/Connexion.java
@@ -0,0 +1,5 @@
+package fr.projet.model.logging;
+
+public class Connexion {
+
+}
---
+++ src/main/java/fr/projet/model/logging/Inscription.java
@@ -0,0 +1,15 @@
+package fr.projet.model.logging;
+
+public class Inscription {
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
