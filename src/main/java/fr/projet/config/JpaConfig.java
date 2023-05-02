28e1dd88efed437c72b881e4dd88bd0ea092bee6
PierreNicolasRey <pnrey@hotmail.fr>
2023-05-02 10:25:50 +0200

    JpaConfig avec les imports

--- src/main/java/fr/projet/config/JpaConfig.java
+++ src/main/java/fr/projet/config/JpaConfig.java
@@ -5,7 +5,9 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
