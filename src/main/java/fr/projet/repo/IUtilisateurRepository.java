6c50463004ac77a96383723d9024f2f42ccc107c
Quentin Goutaland <06612219+quentgoutaland@users.noreply.github.com>
2023-05-02 11:03:33 +0200

    change to JpaRepository

--- src/main/java/fr/projet/repo/IUtilisateurRepository.java
+++ src/main/java/fr/projet/repo/IUtilisateurRepository.java
@@ -2,9 +2,11 @@ package fr.projet.repo;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import fr.projet.model.utilisateur.Utilisateur;
 
-public interface IUtilisateurRepository extends IRepository<Utilisateur, Integer>{
+public interface IUtilisateurRepository extends JpaRepository<Utilisateur, Integer>{
 	public Optional<Utilisateur> findByPseudo(String pseudo);
 	public Optional<Utilisateur> findByPseudoRole(String pseudo, String role);
 }
