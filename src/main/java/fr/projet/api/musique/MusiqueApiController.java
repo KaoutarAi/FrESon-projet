b3019d9f8110c651a73424c2de6dc1ed6bcac83d
Quentin Goutaland <06612219+quentgoutaland@users.noreply.github.com>
2023-05-25 12:43:22 +0200

    CRUD API musique

--- src/main/java/fr/projet/api/musique/MusiqueApiController.java
+++ src/main/java/fr/projet/api/musique/MusiqueApiController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,6 +58,16 @@ public class MusiqueApiController {
         Musique music = request.toMusique();
         music.setId(id);
         return MusiqueResponse.convert(this.repoMusic.save(music));
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable int id) {
+        try {
+            this.repoMusic.deleteById(id);
+        }
+        catch (Exception ex) {
+            throw new MusiqueNotValidException();
+        }
     }
 
 }
