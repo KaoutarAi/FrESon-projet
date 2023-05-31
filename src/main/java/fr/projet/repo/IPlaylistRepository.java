4835d78e5f7dc6a6829b1227290e16e5e907af5e
Quentin Goutaland <06612219+quentgoutaland@users.noreply.github.com>
2023-05-31 14:32:53 +0200

    Home playlists display back

--- src/main/java/fr/projet/repo/IPlaylistRepository.java
+++ src/main/java/fr/projet/repo/IPlaylistRepository.java
@@ -3,7 +3,6 @@ package fr.projet.repo;
 import java.util.List;
 import java.util.Optional;
 
-import org.postgresql.core.NativeQuery;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
