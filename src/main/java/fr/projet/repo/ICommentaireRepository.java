6e4758f4bea58f7dfca030f093b31f51549f2b6e
Quentin Goutaland <06612219+quentgoutaland@users.noreply.github.com>
2023-06-16 03:50:20 +0200

    minor api update

--- src/main/java/fr/projet/repo/ICommentaireRepository.java
+++ src/main/java/fr/projet/repo/ICommentaireRepository.java
@@ -8,27 +8,27 @@ import org.springframework.data.jpa.repository.Query;
 import fr.projet.model.utilisateur.Commentaire;
 
 public interface ICommentaireRepository extends JpaRepository<Commentaire, Integer>{
-	
+
 	@Query("select c from Commentaire c where c.playlist.id = ?1")
 	public List<Commentaire> findAllByPlaylist(int playlistId);
-	
-	@Query("select c from Commentaire c where c.playlist.id = ?1 and c.contenu like %?2%")
+
+	@Query("select c from Commentaire c where c.playlist.id = ?1 and c.contenu like CONCAT('%', ?2, '%')")
 	public List<Commentaire> findAllByPlaylistAndContenuContaining(int playlistId, String contenu);
-	
-	@Query("select c from Commentaire c where c.playlist.id = ?1 and c.utilisateur.pseudo = ?2 and c.contenu like %?3%")
+
+	@Query("select c from Commentaire c where c.playlist.id = ?1 and c.utilisateur.pseudo = ?2 and c.contenu like CONCAT('%', ?3, '%')")
 	public List<Commentaire> findAllByPlaylistAndUtilisateurAndContenuContaining(int playlistId, String pseudo, String contenu);
-	
-	@Query("select c from Commentaire c where c.playlist.id = ?1 and c.utilisateur.pseudo like %?2%")
+
+	@Query("select c from Commentaire c where c.playlist.id = ?1 and c.utilisateur.pseudo like CONCAT('%', ?2, '%')")
 	public List<Commentaire> findAllByPlaylistAndPseudo(int playlistId, String pseudo);
-	
+
 	@Query("select c from Commentaire c where c.playlist.id = ?1 and extract (year from c.date) = ?2 and extract (month from c.date) = ?3 and extract (day from c.date) = ?4 ")
 	public List<Commentaire> findAllByPlaylistAndDateOnly(int playlistId, int annee, int mois, int jour);
-	
-	
+
+
 	@Query("select c from Commentaire c where c.playlist.id = ?1 and c.utilisateur.pseudo = ?2 and extract (year from c.date) = ?3 and extract (month from c.date) = ?4 and extract (day from c.date) = ?5 and extract (hour from c.date) = ?6 and extract (minute from c.date) = ?7")
 	public List<Commentaire> findAllByPlaylistAndUtilisateurAndDate(int playlistId, String pseudo, int annee, int mois, int jour, int heure, int minute);
-	
-	@Query("select c from Commentaire c where c.playlist.id = ?1 and c.utilisateur.pseudo = ?2 and extract (year from c.date) = ?3 and extract (month from c.date) = ?4 and extract (day from c.date) = ?5 and extract (hour from c.date) = ?6 and extract (minute from c.date) = ?7 and c.contenu like %?8%")
+
+	@Query("select c from Commentaire c where c.playlist.id = ?1 and c.utilisateur.pseudo = ?2 and extract (year from c.date) = ?3 and extract (month from c.date) = ?4 and extract (day from c.date) = ?5 and extract (hour from c.date) = ?6 and extract (minute from c.date) = ?7 and c.contenu like CONCAT('%', ?8, '%')")
 	public List<Commentaire> findAllByPlaylistAndUtilisateurAndDateAndContenuContaining(int playlistId, String pseudo, int annee, int mois, int jour, int heure, int minute, String contenu);
 
 }
